535dec1f5e19cbafd5571a20d3e944a130e050bb
soljiju <soljiju@gmail.com>
2025-01-03 17:11:35 +0900

    add ThisJava

--- ThisJava/src/ch02/sec08/CastingExample.java
+++ ThisJava/src/ch02/sec08/CastingExample.java
@@ -2,7 +2,7 @@ package ch02.sec08;
 
 public class CastingExample {
 	public static void main(String[] args) {
-		int var1 = 10;
+		int var1 = 100;
 		byte var2 = (byte) var1;
 		System.out.println(var2);
 		
