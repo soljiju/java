74164d52e19ecbe9952e8fc0e87b3b1748553974
soljiju <soljiju@gmail.com>
2025-01-03 15:34:27 +0900

    ch01 add

---
+++ ch01/src/ch01/Hello.java
@@ -0,0 +1,15 @@
+package ch01;
+
+/*
+ * 날짜:2024/12/31
+ * 이름:주솔지
+ * 내용:ch01.Java 개발환경 구축
+ * 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
