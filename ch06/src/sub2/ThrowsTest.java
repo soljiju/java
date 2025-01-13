780f6ed136c1b51783f63d8c5d7dcac6df3b9a8a
soljiju <soljiju@gmail.com>
2025-01-13 11:19:58 +0900

    ch06

---
+++ ch06/src/sub2/ThrowsTest.java
@@ -0,0 +1,38 @@
+package sub2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2025/01/13
+ * 이름 : 주솔지
+ * 내용 : Java Throws 예외처리 실습하기
+ */
+public class ThrowsTest {
+	
+	public static void main(String[] args)  {
+		
+		try {
+		method1(10);
+		}catch(Exception e)	{
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 정상 종료...");
+	}
+	// method1를 호출한 곳으로 Exception 예외 던짐
+	public static void method1(int n1) throws Exception	{
+		method2(n1);
+	}
+	// method2를 호출한 곳으로 InputMismatchException, ArithmeticException 예외 던짐
+	public static void method2(int n1) throws InputMismatchException, ArithmeticException {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("n2 입력 : ");
+		
+		int n2 = scan.nextInt(); //InputMismatchException 발행
+		int result = n1 / n2; 	 //ArithmeicException 발행
+		
+		System.out.println("result : " + result);
+	}
+}
