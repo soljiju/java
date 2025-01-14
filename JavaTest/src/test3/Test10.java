1f6cdc03cbcac7124581fdf667ed47d4ad456992
soljiju <soljiju@gmail.com>
2025-01-14 17:43:48 +0900

    JavaTest

--- JavaTest/src/test3/Test10.java
+++ JavaTest/src/test3/Test10.java
@@ -1,5 +1,37 @@
 package test3;
 
-public class Test10 {
+/*
+ * 날짜 : 2025/01/13
+ * 이름 : 주솔지
+ * 내용 : 인터페이스 연습문제
+ */
+interface Buyer {
+	public void buy();
+}
 
+interface Seller {
+	public void sell();
+}
+
+class Customers implements Buyer, Seller {
+
+	@Override
+	public void sell() {
+		System.out.println("판매하기");
+	}
+
+	@Override
+	public void buy() {
+		System.out.println("구매하기");
+	}
+}
+
+public class Test10 {
+	public static void main(String[] args) {
+		Buyer  buyer  = new Customers();
+		Seller seller = new Customers();
+		
+		buyer.buy();
+		seller.sell();
+	}
 }
