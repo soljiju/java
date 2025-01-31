55905f4bef2f9457d130ba754597826072a1d336
soljiju <solji@gmail.com>
2025-01-31 17:47:24 +0900

    ShopProject

---
+++ ShopProject/src/controller/ProductController.java
@@ -0,0 +1,70 @@
+
+package controller;
+
+import java.util.List;
+import java.util.Scanner;
+
+import entity.Customer;
+import entity.Product;
+import service.ProductService;
+
+public class ProductController {
+
+	private ProductService service = ProductService.getInstance();
+	
+	public void manage(Scanner sc) {
+		
+		while(true) {
+			System.out.println("------------ 상품관리 ------------");
+			System.out.println("0:메인, 1:상품등록, 2:상품목록, 3:상품검색");
+			System.out.print("선택 : ");
+			
+			int choice = sc.nextInt();
+			
+			switch(choice) {
+			case 0:
+				// 메인 이동
+				return; // 메서드 종료
+			case 1:
+				// 상품등록
+				Product inputProduct = inputProduct(sc);
+				service.registerProduct(inputProduct);
+				
+				System.out.println("상품 등록 완료!");
+				break;
+			case 2:
+				// 상품목록
+				List<Product> products = service.listProduct();
+				
+				for(Product product : products) {
+					System.out.println(product);
+				}
+				break;
+			case 3:
+				// 상품검색
+				break;
+			default:
+				break;
+			} // switch end
+		}// while end
+	}// manage end
+	
+	
+	public Product inputProduct(Scanner sc) {
+		sc.nextLine();
+		System.out.print("상품명 : ");
+		String prodName = sc.nextLine();
+		
+		System.out.print("재고량 : ");
+		int stock = sc.nextInt();
+	
+		System.out.print("가격 : ");
+		int price = sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("제조사 : ");
+		String company = sc.nextLine();
+		
+		return new Product(prodName, stock, price, company);
+	}
+}
---
+++ ShopProject/src/dao/CustomerDAO.java
@@ -0,0 +1,71 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import db.DBHelper;
+import db.SQL;
+import entity.Customer;
+
+public class CustomerDAO extends DBHelper {
+	
+	// 싱글톤
+	private static final CustomerDAO INSTANCE = new CustomerDAO();
+	public static CustomerDAO getInstance() {
+		return INSTANCE;
+	}
+	private CustomerDAO() {}
+		
+	// 기본 CRUD
+	public void insertCustomer(Customer customer) {
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.INSERT_CUSTOMER);
+			psmt.setString(1, customer.getCustId());
+			psmt.setString(2, customer.getName());
+			psmt.setString(3, customer.getHp());
+			psmt.setString(4, customer.getAddr());
+			psmt.executeUpdate();
+			closeAll();		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Customer selectCustomer(String custId) {
+		
+		Customer customer = null; // 선언
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_CUSTOMER);
+			psmt.setString(1, custId);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()) {
+				customer = new Customer(); // 생성
+				customer.setCustId(rs.getString(1));
+				customer.setName(rs.getString(2));
+				customer.setHp(rs.getString(3));
+				customer.setAddr(rs.getString(4));
+				customer.setRdate(rs.getString(5));
+			}
+			closeAll();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return customer; // 리턴
+	}
+	
+	public List<Customer> selectCustomers() {
+		return null;
+	}
+	
+	public void updateCustomer(Customer customer) {
+		
+	}
+	
+	public void deleteCustomer(String custId) {
+		
+	}
+}
